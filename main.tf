6a506d187c57701914629a1873ef2d73d23275dd
subhankar <subhankarn@gmail.com>
2024-03-15 23:55:53 +0530

    uploaded a test file

--- main.tf
+++ main.tf
@@ -38,3 +38,12 @@ resource "azurerm_storage_container" "data" {
     storage_account_name = azurerm_storage_account.nayakstorage1334.name
     container_access_type = "blob"
 }
+
+resource "azurerm_storage_blob" "file" {
+    name = "myfiles.zip"
+    storage_account_name = azurerm_storage_account.nayakstorage1334.name
+    storage_container_name = azurerm_storage_container.data.name
+    type = "Block"
+    source = "../../Downloads/rent.zip"
+  
+}
